6482ee1e9d2e56dcb50c6972f190ee7b6dc3f817
myari211 <syukrihsb148@gmail.com>
2020-03-06 03:11:55 +0700

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import SignIn from "./Pages/SignIn/SignIn"
 import { Router, Switch, Route } from "react-router-dom"
 import { createBrowserHistory } from "history"
 import routes, { IRoute } from "./Routes"
+
+import "react-perfect-scrollbar/dist/css/styles.css"
+import "./assets/scss/index.scss"
+
 const browserHistory = createBrowserHistory()
 
 function App() {
