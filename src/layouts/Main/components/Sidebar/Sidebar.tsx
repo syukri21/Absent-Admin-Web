c98c88eb9a119c21803dce1342165339435e0b04
myari211 <syukrihsb148@gmail.com>
2020-03-24 02:51:11 +0700

    fix sidebar

--- src/layouts/Main/components/Sidebar/Sidebar.tsx
+++ src/layouts/Main/components/Sidebar/Sidebar.tsx
@@ -3,12 +3,15 @@ import clsx from "clsx"
 
 import { Divider, Drawer } from "@material-ui/core"
 import DashboardIcon from "@material-ui/icons/Dashboard"
+import Book from "@material-ui/icons/Book"
+
 import PeopleIcon from "@material-ui/icons/People"
 import SettingsIcon from "@material-ui/icons/Settings"
 
 import { Profile, SidebarNav } from "./components"
 import useStyles from "./styles"
 import PerfectScrollbar from "react-perfect-scrollbar"
+import User from "../../../../provider/User"
 
 export interface SidebarProps {
     className?: string
@@ -42,8 +45,30 @@ const pages: Array<Page> = [
     }
 ]
 
+const pagesAdmin: Array<Page> = [
+    {
+        title: "Course",
+        href: "/",
+        icon: <Book />
+    },
+    {
+        title: "Teacher",
+        href: "/teacher",
+        icon: <PeopleIcon />
+    },
+
+    {
+        title: "Settings",
+        href: "/settings",
+        icon: <SettingsIcon />
+    }
+]
+
 const Sidebar: React.SFC<SidebarProps> = props => {
     const { isOpen, variant, onClose, className, ...rest } = props
+    const [user] = User.useGlobal()
+    console.log("user", user)
+
     const classes = useStyles()
 
     return (
@@ -52,7 +77,7 @@ const Sidebar: React.SFC<SidebarProps> = props => {
                 <div {...rest} className={clsx(classes.root, className)}>
                     <Profile />
                     <Divider className={classes.divider} />
-                    <SidebarNav className={classes.nav} pages={pages} />
+                    {user.data.user && <SidebarNav className={classes.nav} pages={user.data.user.roleId === 1 ? pages : pagesAdmin} />}
                 </div>
             </PerfectScrollbar>
         </Drawer>
