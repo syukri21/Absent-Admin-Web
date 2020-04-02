78384b3aec5237e25e27df3d25565a0500b741d9
myari211 <syukrihsb148@gmail.com>
2020-04-03 05:21:04 +0700

    fix socket

--- src/socket/socket.tsx
+++ src/socket/socket.tsx
@@ -15,31 +15,37 @@ class SocketClient {
     }
 
     public onAbsent(scheduleId: string, fn: Function) {
-        const token = Api.getToken()
-        this.socket = io("http://localhost:3000", {
-            path: "/socket.io",
-            transports: ["websocket"],
-            transportOptions: {
-                websocket: {
-                    extraHeaders: {
-                        "x-clientid": "abc"
+        if (!this.socket) {
+            const token = Api.getToken()
+            this.socket = io("http://localhost:3000", {
+                path: "/socket.io",
+                transports: ["websocket"],
+                transportOptions: {
+                    websocket: {
+                        extraHeaders: {
+                            "x-clientid": "abc"
+                        }
                     }
+                },
+                query: {
+                    token,
+                    protected: "teacher",
+                    room: `absent.${scheduleId}`
                 }
-            },
-            query: {
-                token,
-                protected: "teacher",
-                room: `absent.${scheduleId}`
-            }
-        })
-        this.socket.emit("/join", {
-            name: `absent.${scheduleId}`
-        })
-        this.socket.on(`absent`, fn)
+            })
+            this.socket.emit("/join", {
+                name: `absent.${scheduleId}`
+            })
+            this.socket.on(`absent`, fn)
+        }
     }
 
     off(name: string) {
-        if (this.socket) this.socket.removeListener(name)
+        if (this.socket) {
+            this.socket.removeAllListeners()
+            this.socket.close()
+            this.socket = undefined
+        }
     }
 }
 
